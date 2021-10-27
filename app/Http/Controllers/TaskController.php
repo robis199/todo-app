bea799af77d0f23091c7534f49fe25bfb070eff8
Roberts Cipulis <rcipulis@gmail.com>
2021-10-27 18:12:55 +0300

    Initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class TaskController extends Controller
+{
+    public function show() {
+        print_r("hi hello");
+    }
+}
