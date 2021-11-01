561224a6a95e781ebd362bf6ddc262beea021746
Roberts Cipulis <rcipulis@gmail.com>
2021-11-01 09:58:10 +0200

    laravel-auth-mix-breeze

--- database/migrations/2021_10_27_145925_create_tasks_table.php
+++ database/migrations/2021_10_27_145925_create_tasks_table.php
@@ -11,11 +11,11 @@ class   CreateTasksTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->description();
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -25,7 +25,7 @@ class   CreateTasksTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('tasks');
     }
